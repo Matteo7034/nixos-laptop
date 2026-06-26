f3325734e09a47e7e5abf57ba8dd24bb57d6ad35
Matteo <matteo7034@gmail.com>
2026-06-26 10:56:29 +0200

    adding pywal16

--- modules/pkg.nix
+++ modules/pkg.nix
@@ -7,6 +7,8 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     tree
+    ghostscript
+    pywal16
     wget
 	  clang
 	  kitty
